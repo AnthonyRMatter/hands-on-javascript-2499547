0500cbfd613c74b187ef9b685f0ba70f57ade8db
AnthonyRMatter <134010366+AnthonyRMatter@users.noreply.github.com>
2023-08-18 23:12:01 +0000

    final_commit

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 export default {
   // Get API key from https://home.openweathermap.org/users/sign_up
   appid: "51faf1d21b3a9299897dfc5bafea90b5",
-  units: "metric",
-  location: "fullerton,ca",
+  units: "imperial",
+  location: "fullerton,ca,us",
 };
